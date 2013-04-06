87a7927a4f010d2b5fa3c0d536dd73cd03189784
David Hahn <davidmichaelhahn@gmail.com>
2013-04-06 16:50:17 -0500

    Remove address association from people.

--- app/models/person.rb
+++ app/models/person.rb
@@ -15,7 +15,6 @@ class Person < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :city, :state, :street, :zip
   belongs_to :representative, class_name: "Person", foreign_key: :representative_id
   has_many :represents, class_name: "Person", foreign_key: :representative_id
-  has_one :address
   has_and_belongs_to_many :skills
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :services
