b91292928f8918d8e2b8e496e70d6ce7132bfd75
David Hahn <msustudentemails@gmail.com>
2013-04-20 15:21:37 -0500

    Finish prototype of people form.(including donations)

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,13 +12,14 @@
 #
 
 class Person < ActiveRecord::Base
-  attr_accessible :email, :name, :city, :state, :street, :zip, :skill_ids, :group_ids, :service_ids, :representative_id, :represent_ids
+  attr_accessible :email, :name, :city, :state, :street, :zip, :skill_ids, :group_ids, :service_ids, :representative_id, :represent_ids,  :cash_donations_attributes
   belongs_to :representative, class_name: "Person", foreign_key: :representative_id
   has_many :represents, class_name: "Person", foreign_key: :representative_id
   has_and_belongs_to_many :skills
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :services
   has_many :cash_donations, dependent: :destroy
+  accepts_nested_attributes_for :cash_donations, :reject_if => lambda {|t| t[:date].blank? or t[:amount].blank? }, allow_destroy: true
   validate :either_name_or_email
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z|^$/i
 
--- app/models/service.rb
+++ app/models/service.rb
@@ -17,7 +17,7 @@
 #
 
 class Service < ActiveRecord::Base
-  attr_accessible :date, :description, :hours, :notes, :returned_survey, :sent_survey, :trained, :willing_to_return, :place_id, :person_ids
+  attr_accessible :date, :description, :hours, :notes, :returned_survey, :sent_survey, :trained, :willing_to_return, :place_id, :person_ids, :group_ids
   has_and_belongs_to_many :people
   has_many :group_services, dependent: :destroy
   has_many :groups, through: :group_services
