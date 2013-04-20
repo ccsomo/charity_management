e8d103da4a3ec0d8918821196ae5d5b133c693f3
David Hahn <msustudentemails@gmail.com>
2013-04-20 15:32:58 -0500

    finish group _form.html.haml (except members_served which is going to require a bit more logic).

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,11 +10,12 @@
 #
 
 class Group < ActiveRecord::Base
-  attr_accessible :members, :name, :person_ids
+  attr_accessible :members, :name, :person_ids, :cash_donations_attributes
   has_and_belongs_to_many :people
   has_many :group_services, dependent: :destroy
   has_many :services, through: :group_services
   has_many :cash_donations, dependent: :destroy
+  accepts_nested_attributes_for :cash_donations, :reject_if => lambda {|t| t[:date].blank? or t[:amount].blank? }, allow_destroy: true
 
   validates_presence_of :name
 end
